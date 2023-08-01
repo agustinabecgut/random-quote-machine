8477bcff8b605af6245b185f6392fcea01e9ef94
agustinabecgut <agustina.b.becerra@gmail.com>
2023-08-01 00:43:34 -0300

    Implementation of QuoteMachine component into App.js, and started styling

---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import '../App.scss'
+
+function QuoteMachine() {
+  const [content, setQuote] = useState('');
+  const [author, setAuthor] = useState('');  
+  
+  useEffect(() => {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+        // Extract the quote and author from the API response
+        const newQuote = data.content;
+        const newAuthor = data.author;
+  
+        // Update the state with the new quote and author
+        setQuote(newQuote);
+        setAuthor(newAuthor);
+      })
+      .catch(error => console.error('Error fetching quote:', error));
+  }, []);  
+
+  const handleNewQuote = () => {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+        
+        const newQuote = data.content;
+        const newAuthor = data.author;
+        setQuote(newQuote);
+        setAuthor(newAuthor);
+      })
+      .catch(error => console.error('Error fetching quote:', error));
+  };
+
+  return (
+    <div id="quote-box" className='quote-box'>
+      <p id="text" className='text'>"{content}"</p>
+      <p id="author" className='author'>- {author}</p>
+      <button id="new-quote" className='new-quote' onClick={handleNewQuote}>New Quote</button>
+      <a href="twitter.com/intent/tweet" id="tweet-quote" className='tweet-quote'><i className="fa-brands fa-twitter"></i></a>
+      <footer className='footer'>
+        <p className='footer-content'>by ABdev</p>
+      </footer>
+    </div>
+  );
+  
+}
+
+export default QuoteMachine;
